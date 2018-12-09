dbdba98725cb8ce32734d05ed1cc396d2412e198
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2018-12-09 05:42:49 +0200

    main setups

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,11 +1,20 @@
 import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Route }
+// sample
+// <Router>
+//    <div>
+//      <Route exact path="/" component={Home} />
+//      <Route path="/about" component={About} />
+//      <Route path="/topics" component={Topics} />
+//    </div>
+//  </Router>
 
-
-const Router = () => (
-  <Fragment>
-
-  </Fragment>
-
-
-);
-export default Router;
+// const Router = () => (
+//   <Fragment>
+//       <Route exact path="/" component={Home} />
+//       <Route path="/about"  component={About} />
+//       <Route path="/topics" component={Topics} />
+//   </Fragment>
+//
+// );
+// export default Router;
---
+++ src/selectors/selector.js
@@ -0,0 +1,1 @@
+// here we'll store functions that will return an arrays to us.
